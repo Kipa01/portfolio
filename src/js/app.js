25f38dbc81012490a9e9e1d4bc10fbf926e62622
KipaFlat12 <palkin.k@flat12.ru>
2022-09-12 14:09:57 +0300

    animations on scroll

--- src/js/app.js
+++ src/js/app.js
@@ -11,11 +11,13 @@ import "./libs/tag-canvas.js";
 // Подключение файлов со своим кодом
 
 import "./files/background-animation.js";
+import "./files/aos.js";
 import "./files/custom-scrollbar.js";
 import "./files/smooth-site-scroll.js";
 import "./files/hide-header-on-scroll.js";
 import "./files/skills-animation.js";
 import "./files/slider.js";
 import "./files/anchorsScroll.js";
+// import "./files/on-scroll-animations.js";
 
 // /Подключение файлов со своим кодом
---
+++ src/js/files/aos.js
@@ -0,0 +1,9 @@
+import AOS from "aos/dist/aos.js";
+
+AOS.init({
+	offset: 100,
+	duration: 600,
+	easing: 'ease-in-out',
+	delay: 0,
+	once: true
+});
